ba3600528a95810c93a30506e17445704fc44fe1
Yoeri Pasmans <yoerip@live.nl>
2018-02-06 11:55:57 +0100

    Gulp setup

---
+++ gulpfile.js
@@ -0,0 +1,15 @@
+var gulp = require('gulp');
+var browserSync = require('browser-sync').create();
+
+gulp.task('browserSync', function() {
+  browserSync.init({
+    server: {
+      baseDir: 'app'
+    },
+});
+});
+
+gulp.task('watch', ['browserSync'], function (){
+  gulp.watch('app/**/*.css', browserSync.reload);
+  // Other watchers
+});

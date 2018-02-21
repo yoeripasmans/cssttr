d615ab4645b7fb8af7b4c7cf0f33d2f0c65bd141
Yoeri Pasmans <yoerip@live.nl>
2018-02-21 18:17:58 +0100

    Latest changes

---
+++ app/scripts/index.js
@@ -0,0 +1,12 @@
+//Checkbox close
+document.querySelector("#cart-toggle-close").addEventListener("click", function() {
+	document.querySelector("#cart-toggle").checked = false;
+});
+
+document.querySelector("#login-toggle-close").addEventListener("click", function() {
+	document.querySelector("#login-toggle").checked = false;
+});
+
+document.querySelector("#detail-toggle-close").addEventListener("click", function() {
+	document.querySelector("#detail-toggle").checked = false;
+});
